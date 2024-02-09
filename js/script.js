a2c487206c0a4423c12d93e511bd271448cab9b0
3ri4nG0ld <brianldg92@gmail.com>
2024-02-09 19:54:35 +0000

    Added Dark mode button and Blog link

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 function redirectToWebmin() {
     window.location.href = "https://3ri4nG0ld.duckdns.org:10000";
 }
+function switchDarkMode(){
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+}
